2da351b1c6983f2d658f0f9b5ddd9b1bd532989c
yosiakatsuki <y.ogata@yosiakatsuki.net>
2019-03-13 23:38:25 +0900

    first commit

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Ys_Wp_Gutenberg_Switcher
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
---
+++ ys-wp-gutenberg-switcher.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Ys Wp Gutenberg Switcher
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ys-wp-gutenberg-switcher
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ys_Wp_Gutenberg_Switcher
+ */
